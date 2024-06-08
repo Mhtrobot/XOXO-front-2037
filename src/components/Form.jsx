d2ee0de6cbcf4afe3e3a19f96cd9f109fa8913b1
Mhtrobot <mhtrobot760@gmail.com>
2024-06-08 23:49:35 +0330

    loginPage backend finish

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,38 @@
-const Form = () => {
-  return <form>
-      <input type="text" name="username" placeholder="Enter Player One"/>
+import {useState} from "react";
+
+const Form = ({setIsLogged}) => {
+    const [user1, setUser1] = useState('');
+    const [user2, setUser2] = useState('');
+    async function loginUser(e){
+        e.preventDefault();
+
+        const data = {
+            user1: user1,
+            user2: user2
+        }
+
+        const response = await fetch(' http://127.0.0.1:8000/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+        if (response.status === 200) {
+            setIsLogged(true)
+        }
+        else
+            alert("Error! Either user 1 or 2 incorrect")
+    }
+
+  return <form onSubmit={loginUser}>
+      <input type="text" name="user1" placeholder="Enter Player One" value={user1}
+      onChange={(event)=>setUser1(event.target.value)}/>
       <br/>
-      <input type="text" name="username" placeholder="Enter Player Two"/>
+      <input type="text" name="user2" placeholder="Enter Player Two" value={user2}
+             onChange={(event)=>setUser2(event.target.value)}/>
       <br/>
       <button type="submit" className="btn">START GAME</button>
   </form>
--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,18 +1,24 @@
 import HeaderPage from "./HeaderPage.jsx";
 import FooterPage from "./FooterPage.jsx";
 import Form from "./Form.jsx";
+import {useState} from "react";
+import Game from "./Game.jsx";
 
 const LoginPage = () => {
-  return(
-      <>
-          <HeaderPage />
-          <div className="login-form">
-              <p>Fill the Form</p>
-              <Form />
-          </div>
-          <FooterPage />
-      </>
-  );
+    const [isLogged, setIsLogged] = useState(false)
+
+    if (isLogged)
+        return <Game />
+    return(
+        <>
+            <HeaderPage />
+            <div className="login-form">
+                <p>Fill the Form</p>
+                <Form setIsLogged={setIsLogged} />
+            </div>
+            <FooterPage />
+        </>
+    );
 }
 
 export default LoginPage;
